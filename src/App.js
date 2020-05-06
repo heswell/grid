fb2412fce1028f670b4429d9835c60c610030593
heswell <steve@heswell.com>
2020-05-06 08:09:22 +0100

    feat(structure): basic shell
    Basic structure of grid with  2 way scrolling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Grid } from "./grid";
 import "./styles.css";
 
+/**
+ * First Cut
+ */
 export default function App() {
   return <Grid height={600} width={800} headerHeight={32} />;
 }
